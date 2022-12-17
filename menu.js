d09268a4b643b76d00ca91358caed9c2ba3532d6
raoakif <akifrao@gmail.com>
2022-12-17 19:31:50 +0500

    Lecture 1

---
+++ menu.js
@@ -0,0 +1,45 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log("😃Welcome To Banking Application😃");
+console.log("\n 1. Create new account")
+console.log("\n 2. Deposit Money")
+console.log("\n 3. Withdraw Money")
+console.log("\n 4. Check Balance")
+console.log("\n 5. Transfer Money")
+console.log("\n 6. Exit")
+
+const input = () => {
+  return new Promise((resolve, reject) => {
+    rl.question('\n 👉Enter Your Choice: ', (ch) => {
+      resolve(ch)
+    })
+  })
+}
+
+const start = async () => {
+  while(true) {
+    const choice = await input()
+
+    if(choice == 1) {
+      console.log(`\n ✔️ Please Create a new Account`)
+    } else if (choice == 2) {
+      console.log(`\n ✔️ Please Deposit Money`)
+    } else if (choice == 3) {
+      console.log(`\n ✔️ Please Withdraw Money`)
+    } else if (choice == 4) {
+      console.log(`\n ✔️ Please Check Balance`)
+    } else if (choice == 5) {
+      console.log(`\n ✔️ Please Transfer Money`)
+    } else {
+      console.log(`Bye Bye`)
+      process.exit()
+    }
+  }
+}
+
+start()
